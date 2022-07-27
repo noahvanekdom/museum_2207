8b0f6bdda2f252f69606453490def16700411509
Noah van Ekdom <noahvanekdom@gmail.com>
2022-07-27 09:35:09 -0600

    Add first museum tests

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -1,4 +1,19 @@
+require './lib/patron'
+require './lib/exhibit'
 require './lib/museum'
 
-RSpec.describe Museum do 
+RSpec.describe Museum do
+    let(:dmns) {Museum.new("Denver Museum of Nature and Science")}
+    it 'exists' do
+        expect(dmns).to be_instance_of Museum
+    end
+
+    xit 'has a name' do
+         expect(dmns.name).to eq "Denver Museum of Nature and Science"
+    end
+
+    xit 'initially returns an empty array of exhibits' do
+         expect(dmns.exhibits).to eq []
+    end
+
 end
