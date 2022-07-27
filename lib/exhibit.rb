c60721b2688e63f69022519388f24d1bd44d427f
Noah van Ekdom <noahvanekdom@gmail.com>
2022-07-27 09:28:35 -0600

    Add exhibit class

--- lib/exhibit.rb
+++ lib/exhibit.rb
@@ -1,2 +1,14 @@
 class Exhibit
+    attr_reader :details
+    def initialize(details)
+        @details = details
+    end
+
+    def name
+        @details[:name]
+    end
+
+    def cost
+        @details[:cost]
+    end
 end
