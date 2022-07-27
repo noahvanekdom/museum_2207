269b48f989736245ef5a20aea307623240fd18ee
Noah van Ekdom <noahvanekdom@gmail.com>
2022-07-27 09:18:44 -0600

    Add iteration 1 tests

--- spec/exhibit_spec.rb
+++ spec/exhibit_spec.rb
@@ -1,4 +1,14 @@
 require './lib/exhibit'
 
-RSpec.describe Patron do
+RSpec.describe Exhibit do
+    let(:exhibit) {Exhibit.new({name: "Gems and Minerals", cost: 0})}
+
+    it 'has a name' do
+        expect(exhibit.name).to eq "Gems and Minerals"
+    end
+
+    it 'has a cost' do
+        expect(exhibit.cost).to eq 0
+    end
+
 end
--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -1,4 +1,22 @@
 require './lib/patron'
 
 RSpec.describe Patron do
+    let(:patron_1) {Patron.new("Bob", 20)}
+
+    it 'has a name' do
+        expect(patron_1.name).to eq "Bob"
+    end
+
+    it 'has money to spend' do
+        expect(patron_1.spending_money).to eq 20
+    end
+
+    it 'can return an array of interests' do
+        expect(patron_1.interests).to eq []
+
+        patron_1.add_interest("Dead Sea Scrolls")
+        patron_1.add_interest("Gems and Minerals")
+
+        expect(patron_1.interests).to eq ["Dead Sea Scrolls", "Gems and Minerals"]
+    end
 end
